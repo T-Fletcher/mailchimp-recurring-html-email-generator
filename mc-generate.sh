503e37d93da9c72ab555970b84026f323a293f0a
Tim Fletcher <timfletcher88@gmail.com>
2024-08-19 09:14:44 +1000

    Added test for code alias before running it

--- mc-generate.sh
+++ mc-generate.sh
@@ -229,9 +229,10 @@ function cleanUp() {
     COMPLETE_TIME=$(useDate +%s)
     logInfo "Finished $FULL_NAME in $(($COMPLETE_TIME - $NOW_EPOCH)) seconds, at $(useDate -u +"%Y%m%dT%H:%M:%S%z")"
     
-    if [[ $DEBUG == "true" ]]; then
-        logDebug "Opening log file from this run (requires 'code' alias to open your editor of choice)..."
-        code $MAILCHIMP_SCRIPT_LOGFILE
+    if [[ $DEBUG == "true" ]] && ! code >/dev/null 2>&1; then
+            logDebug "Opening log file from this run (requires 'code' alias to open your editor of choice)..."
+            code $MAILCHIMP_SCRIPT_LOGFILE
+        fi
     fi
     return 0
 }
